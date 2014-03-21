5ac4473249e238b66c625ba7dd2e4421053c71c9
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-21 10:28:07 +0100

    Added responses.

---
+++ src/main/java/de/metalcon/sdd/api/responses/SddCouldNotQueueResponse.java
@@ -0,0 +1,9 @@
+package de.metalcon.sdd.api.responses;
+
+import de.metalcon.api.responses.SuccessResponse;
+
+public class SddCouldNotQueueResponse extends SuccessResponse {
+
+    private static final long serialVersionUID = -8856281553160944283L;
+
+}
---
+++ src/main/java/de/metalcon/sdd/api/responses/SddSucessfulReadResponse.java
@@ -0,0 +1,9 @@
+package de.metalcon.sdd.api.responses;
+
+import de.metalcon.api.responses.SuccessResponse;
+
+public class SddSucessfulReadResponse extends SuccessResponse {
+
+    private static final long serialVersionUID = -5596683508242175935L;
+
+}
