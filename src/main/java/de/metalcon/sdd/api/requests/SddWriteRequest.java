9b64752595787e76754ea651ffa60e937e7f47a8
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-21 13:44:06 +0100

    Parameter names more meaningful.

--- src/main/java/de/metalcon/sdd/api/requests/SddWriteRequest.java
+++ src/main/java/de/metalcon/sdd/api/requests/SddWriteRequest.java
@@ -9,25 +9,25 @@ public class SddWriteRequest extends SddRequest {
 
     private static final long serialVersionUID = 284025577561283163L;
 
-    public void setProperties(Muid id, Map<String, String> properties) {
+    public void setProperties(Muid nodeId, Map<String, String> properties) {
     }
 
-    public void setRelation(Muid id, String type, Muid relationId) {
+    public void setRelation(Muid nodeId, String type, Muid toId) {
     }
 
-    public void setRelations(Muid id, String type, List<Muid> relIds) {
+    public void setRelations(Muid nodeId, String type, List<Muid> toIds) {
     }
 
-    public void addRelations(Muid id, String type, List<Muid> relIds) {
+    public void addRelations(Muid nodeId, String type, List<Muid> toIds) {
     }
 
-    public void delete(Muid id) {
+    public void delete(Muid nodeId) {
     }
 
-    public void deleteRelation(Muid id, String type, Muid relId) {
+    public void deleteRelation(Muid nodeId, String type, Muid toId) {
     }
 
-    public void deleteRelations(Muid id, String type, List<Muid> relIds) {
+    public void deleteRelations(Muid nodeId, String type, List<Muid> toIds) {
     }
 
 }
