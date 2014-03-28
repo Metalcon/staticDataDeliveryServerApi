4b484f21dc7263d4980b435196b9babe98f96e1d
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-03-28 10:49:47 +0100

    Moved Expcetion hierarchy into Api repo.

---
+++ src/main/java/de/metalcon/sdd/api/exception/InvalidNodeTypeException.java
@@ -0,0 +1,7 @@
+package de.metalcon.sdd.api.exception;
+
+public class InvalidNodeTypeException extends SddUsageException {
+
+    private static final long serialVersionUID = -7663697597797325081L;
+
+}
